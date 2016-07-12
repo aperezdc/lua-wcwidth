b379a5e3679a2cb5e5580cac15a1e8253aa24e64
Adrian Perez de Castro <aperez@igalia.com>
2016-07-12 22:20:14 +0300

    Generate tables with their values sorted
    This will allow for using binary search instead of linear search.

---
+++ spec/unicodetables_spec.lua
@@ -0,0 +1,29 @@
+#! /usr/bin/env lua
+--
+-- unicodetables_spec.lua
+-- Copyright (C) 2016 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+local function check_table(t, step)
+   for i = 1, #t - step, step do
+      assert(t[i] <= t[i + step])
+   end
+end
+
+describe("wcwidth.zerotab", function ()
+   local zerotab = require "wcwidth.zerotab"
+   it("is sorted", function ()
+      check_table(zerotab, 1)
+      check_table(zerotab, 2)
+   end)
+end)
+
+describe("wcwidth.widetab", function ()
+   local widetab = require "wcwidth.widetab"
+   it("is sorted", function ()
+      check_table(widetab, 1)
+      check_table(widetab, 2)
+   end)
+end)
