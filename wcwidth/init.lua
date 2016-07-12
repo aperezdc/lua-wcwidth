87caff2c081ae90214fb32e2cd6be7b2800935f5
Adrian Perez de Castro <aperez@igalia.com>
2016-07-12 23:30:10 +0300

    Replace linear search with binary search
    This gives a small speed improvement by using binary search. Most of the code
    in this patch handles implementing halfodd() for different Lua versions, which
    is used to calculate the next middle point for the binary search. The function
    calculates the division of a number by two, and rounds it up to the next odd
    number. This is because data tables have the following shape:
    
      tab = {
         lowbound1, upperbound1,
         lowbound2, upperbound2,
         ...
         lowboundN, upperboundN,
      }
    
    The low bound of each rune range is always in an odd table index. The binary
    search starts with the 1 as the index of the lower, and #tab as the upper
    bound. The lower bound always contains odd indexes, and the upper bound always
    contains even indexes.
    
    The halfodd() function returns is used to calculate an odd middle index, and
    then the rune being tested is compared with the values at tab[middle] (low
    bound of the range) and tab[middle+1] (upper bound of the range). On each
    iteration either the upper bound is updated to middle-1, or the lower bound to
    middle+2, to maintain the invariant that lower bound indexes are always odd,
    and upper bound indexes are always even.
    
    Binary search increases the speed of wcwidth() from 0.566 milliseconds per
    function call (average) to just 0.061 milliseconds. This is a 9.3x speedup.

--- wcwidth/init.lua
+++ wcwidth/init.lua
@@ -6,19 +6,63 @@
 -- Distributed under terms of the MIT license.
 --
 
-local _tab_zero = require "wcwidth.zerotab"
-local _tab_wide = require "wcwidth.widetab"
+--
+-- Divides a number by two, and rounds it up to the next odd number.
+--
+local halfodd = (function ()
+   -- Lua 5.3 has bit shift operators and integer division.
+   local ok, f = pcall(function ()
+      return load("return function (x) return (x >> 1) | 1 end")()
+   end)
+   if ok then return f end
+   -- Try using a bitwise manipulation module.
+   for _, name in ipairs { "bit32" } do -- "bit", "bit32" } do
+      local ok, m = pcall(require, name)
+      if ok then
+         local rshift, bor = m.rshift, m.bor
+         return function (x) return bor(rshift(x, 1), 1) end
+      end
+   end
+   -- Fall-back using the math library.
+   -- Lua 5.1 has math.mod() instead of the "%" operator.
+   local floor = math.floor
+   if (pcall(function () return load("return 10 % 3")() == 4 end)) then
+      return function (x)
+         local r = floor(x / 2)
+         return (r % 2 == 0) and r + 1 or r
+      end
+   end
+   local mod = math.mod or function (x, d)
+      while x > d do x = x - d end
+      return x
+   end
+   return function (x)
+      local r = floor(x / 2)
+      return mod(r, 2) == 0 and r + 1 or r
+   end
+end)()
 
 local function _lookup(rune, table)
-   -- TODO: Change the linear search to binary search.
-   for i = 1, #table, 2 do
-      local lo, hi = table[i], table[i + 1]
-      if lo <= rune and rune <= hi then
+   local l, r = 1, #table
+   while l <= r do
+      local m = halfodd(l + r)
+      -- Invariants:
+      -- assert(l % 2 == 1, "lower bound index is not odd")
+      -- assert(r % 2 == 0, "upper bound index is not even")
+      -- assert(m % 2 == 1, "middle point index is not odd")
+      if rune < table[m] then
+         r = m - 1
+      elseif rune > table[m + 1] then
+         l = m + 2
+      else
          return 1
       end
    end
    return 0
 end
+
+local _tab_zero = require "wcwidth.zerotab"
+local _tab_wide = require "wcwidth.widetab"
 
 local function wcwidth (rune)
    if rune == 0 or
