9eb9f8e7ed5e6c66754e60ee52b8bb63c2afafc2
Adrian Perez de Castro <aperez@igalia.com>
2016-07-13 17:13:41 +0300

    Enable usage of the "bit" module where available
    This gives a small speedup, especially LuaJIT which compiles bitwise
    operations from its "bit" module to machine code.

--- wcwidth/init.lua
+++ wcwidth/init.lua
@@ -15,7 +15,7 @@ local halfodd = (function ()
    end)
    if ok then return f end
    -- Try using a bitwise manipulation module.
-   for _, name in ipairs { "bit32" } do -- "bit", "bit32" } do
+   for _, name in ipairs { "bit", "bit32" } do
       local ok, m = pcall(require, name)
       if ok then
          local rshift, bor = m.rshift, m.bor
